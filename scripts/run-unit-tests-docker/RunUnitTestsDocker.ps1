4ecb4cc5ad8d11523a13ec9de0ec08849f142eb2
Krishna Sharma <krishna.sharma@ni.corp.natinst.com>
2026-02-26 09:24:22 +0530

    Update script to print info [REQ-41]

--- scripts/run-unit-tests-docker/RunUnitTestsDocker.ps1
+++ scripts/run-unit-tests-docker/RunUnitTestsDocker.ps1
@@ -104,9 +104,9 @@ try {
     Write-Information "Setting up VIPM and LUnit in container..." -InformationAction Continue
     
     $setupCmd = if ($VIPMConfigDir) {
-        "`$InformationPreference = 'Continue'; Set-Location C:\scripts; .\SetupLUnit.ps1 -LVVersion $LVVersion -LVBitness $LVBitness -VIPMConfigDir 'C:\vipm-config' -Verbose"
+        "`$InformationPreference = 'Continue'; Set-Location C:\scripts; .\SetupLUnit.ps1 -LVVersion $LVVersion -LVBitness $LVBitness -VIPMConfigDir 'C:\vipm-config' -Verbose *>&1"
     } else {
-        "`$InformationPreference = 'Continue'; Set-Location C:\scripts; .\SetupLUnit.ps1 -LVVersion $LVVersion -LVBitness $LVBitness -Verbose"
+        "`$InformationPreference = 'Continue'; Set-Location C:\scripts; .\SetupLUnit.ps1 -LVVersion $LVVersion -LVBitness $LVBitness -Verbose *>&1"
     }
     
     $setupArgs = $baseDockerArgs + @(
@@ -132,7 +132,7 @@ try {
         $testScriptCmd += " -OpenProjectBeforeRun"
     }
     
-    $testScriptCmd += " -Verbose"
+    $testScriptCmd += " -Verbose *>&1"
 
     $testArgs = $baseDockerArgs + @(
         'powershell.exe', '-NoProfile', '-ExecutionPolicy', 'Bypass', '-Command', $testScriptCmd
